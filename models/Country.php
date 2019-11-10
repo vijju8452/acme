bad3eae8c9022bbc746300fe4687bc622da4dd62
vijju8452 <basuvijaykumar@gmail.com>
2019-11-11 00:17:08 +0530

    fixing #5: we created models for tables created country controller

---
+++ models/Country.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "country".
+ *
+ * @property string $id
+ * @property string $code
+ * @property string $name
+ * @property int $phonecode
+ * @property string $lat
+ * @property string $lng
+ *
+ * @property PhoneNumber[] $phoneNumbers
+ */
+class Country extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'country';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['code', 'name', 'phonecode', 'lat', 'lng'], 'required'],
+            [['phonecode'], 'integer'],
+            [['code'], 'string', 'max' => 2],
+            [['name'], 'string', 'max' => 80],
+            [['lat', 'lng'], 'string', 'max' => 45],
+            [['code'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'code' => 'Code',
+            'name' => 'Name',
+            'phonecode' => 'Phonecode',
+            'lat' => 'Lat',
+            'lng' => 'Lng',
+            'act' => 'Action',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPhoneNumbers()
+    {
+        return $this->hasMany(PhoneNumber::className(), ['country_id' => 'id']);
+    }
+}
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -39,6 +39,10 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
+            Yii::$app->user->isGuest ? (
+                ''
+            ) : (['label' => 'Countries', 'url' => ['/country/index']]),
+            
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
